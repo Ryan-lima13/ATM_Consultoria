ce3271e9246351c0024ee1f1c34dbbfe1f73643e
Ryan-lima13 <79115469+Ryan-lima13@users.noreply.github.com>
2021-10-07 23:16:00 -0300

    "finalizando layout"

--- app/src/main/java/com/example/atm/MainActivity.java
+++ app/src/main/java/com/example/atm/MainActivity.java
@@ -41,7 +41,10 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow)
+                R.id.nav_principal, R.id.nav_servicos, R.id.nav_clientes, R.id.nav_contatos,
+                R.id.nav_sobre
+
+                )
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
